47b3b4f2a709ab7b046b5a92b26a72e77b12eb21
jenriquez88 <jenriquezv88@hotmail.com>
2024-03-02 19:43:47 -0600

    Prep3 Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,24 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int magic = int.Parse(Console.ReadLine());
-        
-        Console.Write("What is your guess? ");
-        int guess = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+        int guess = 0;
+        do 
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
 
-        if (guess == magic)
-        {
-            Console.WriteLine("You guessed it!");
-        }
-        else if (guess < magic)
-        {
-            Console.WriteLine("Higher");
-        }
-        else
-        {
-            Console.WriteLine("Lower");
-        }
+            if (guess == magic)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (guess != magic);
+        
 
     }
 }
