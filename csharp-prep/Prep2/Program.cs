5aed431b8af5ccc72121c27596dfa34226086d33
jenriquez88 <jenriquezv88@hotmail.com>
2024-03-02 19:33:04 -0600

    C# Prep3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradePercentage = int.Parse(grade);
+        string letter = "";
+        if (gradePercentage >=90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >=80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >=70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >=60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+        if (gradePercentage >=70)
+        {
+            Console.WriteLine("Congratulations, you pass the class.");
+        }
+        else 
+        {
+            Console.WriteLine("You failed. Try harder next time.");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magic = int.Parse(Console.ReadLine());
+        
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        if (guess == magic)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        else if (guess < magic)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("Lower");
+        }
+
     }
 }
