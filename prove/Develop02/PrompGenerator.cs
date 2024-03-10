9401327d09c5982270932eda711ebf4388d417a1
jenriquez88 <jenriquezv88@hotmail.com>
2024-03-09 22:35:12 -0600

    prompts

--- prove/Develop02/PrompGenerator.cs
+++ prove/Develop02/PrompGenerator.cs
@@ -6,7 +6,10 @@ public class PromptGenerator
         "What was the best part of my day?",
         "How did I see the hand of the Lord in my life today?",
         "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?"
+        "If I had one thing I could do over today, what would it be?",
+        "Did you felt the Holy Ghost today? Tell me about it?",
+        "Tell me about a moment when you had the opportunity to serve someone",
+        "What do you feel grateful today?"
     };
 
     public string GetRandomPrompt()
